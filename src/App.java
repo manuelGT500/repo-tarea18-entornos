43d293f704be62db4577c967d416aae67629d77a
manuelGT500 <manucb54@gmail.com>
2024-04-24 15:47:46 +0200

    modificacion feature

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("primer commit");
+        System.out.println("rama feature");
     }
 }
